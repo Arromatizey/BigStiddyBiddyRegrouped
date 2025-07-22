9e9d18f897f7a9b9088631d33123225b4fd8c33e
sanaazn <sanaazn10@gmail.com>
2025-07-22 11:42:54 +0200

    feat: add route to display user friendships

--- backend/src/main/java/com/esgi/studyBuddy/repository/FriendshipRepository.java
+++ backend/src/main/java/com/esgi/studyBuddy/repository/FriendshipRepository.java
@@ -2,6 +2,7 @@ package com.esgi.studyBuddy.repository;
 
 import com.esgi.studyBuddy.model.Friendship;
 import com.esgi.studyBuddy.model.FriendshipId;
+import com.esgi.studyBuddy.model.FriendshipStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -12,4 +13,9 @@ import java.util.UUID;
 public interface FriendshipRepository extends JpaRepository<Friendship, FriendshipId> {
     List<Friendship> findByRequester_IdOrTarget_Id(UUID requesterId, UUID targetId);
     Optional<Friendship> findByRequesterIdAndTargetId(UUID requesterId, UUID targetId);
+    List<Friendship> findByStatusAndRequesterIdOrStatusAndTargetId(
+            FriendshipStatus status1, UUID requesterId,
+            FriendshipStatus status2, UUID targetId
+    );
+
 }
